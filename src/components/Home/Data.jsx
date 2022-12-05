996ec91f466fc74c80b6840cb583a3df65e64e74
darko905 <59891655+darko905@users.noreply.github.com>
2022-12-05 17:52:54 +0100

    Create Home component

---
+++ src/components/Home/Data.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Data = () => {
+    return (
+        <div className='home__data'>
+            <h1 className='home__title'>Laki prevoz</h1>
+            <h3 className='home__subtitle'>Sa nama prevoz postaje putovanje</h3>
+            <button className='button button-flex'>Kontakt <i className="uil uil-phone-pause"></i></button>
+        </div>
+    )
+}
+
+export default Data
