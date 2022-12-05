a4ed785e4c23d858ec8db34fefd323745a94b7ae
darko905 <59891655+darko905@users.noreply.github.com>
2022-12-05 17:55:05 +0100

    Create Home component

--- src/components/Home/Social.jsx
+++ src/components/Home/Social.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const Social = () => {
     return (
         <div className='home__social'>
-            <a href='https://www.linkedin.com/in/darko-djukic-3a54b822b' aria-label='Linkedin' className='home__social-icon' target="_blank">
+            <a href='' aria-label='facebook' className='home__social-icon' target="_blank">
                 <i className="uil uil-facebook"></i>
             </a>
-            <a href='https://github.com/darko905' className='home__social-icon' aria-label='Github' target="_blank">
+            <a href='' className='home__social-icon' aria-label='email' target="_blank">
                 <i className="uil uil-envelope"></i>
             </a>
-            <a href='https://instagram.com/djukicdarko?utm_medium=copy_link' aria-label='instagram' className='home__social-icon' target="_blank">
+            <a href='' aria-label='instagram' className='home__social-icon' target="_blank">
                 <i className="uil uil-instagram-alt"></i>
             </a>
         </div>
